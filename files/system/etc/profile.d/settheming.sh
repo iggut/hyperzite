630433d43968a785884bc9cc70e0dc6b421e7344
Igor G <igor.gutchin@gmail.com>
2024-08-31 13:07:01 -0400

    theme fix + missing apps.

--- files/system/etc/profile.d/settheming.sh
+++ files/system/etc/profile.d/settheming.sh
@@ -1,3 +1,5 @@
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' 
+gsettings set org.gnome.desktop.interface gtk-theme 'Tokyonight-Dark-Compact' 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface icon-theme 'kora'
+gsettings set org.gnome.desktop.interface icon-theme 'Flat-Remix-Blue-Dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
+gsettings set org.gnome.desktop.interface cursor-size 20
