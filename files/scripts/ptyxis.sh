6b74b21ec7b301678de2ebc2c15d145c53af0e0a
Igor G <igor.gutchin@gmail.com>
2024-11-01 10:36:36 -0400

    1

--- files/scripts/ptyxis.sh
+++ files/scripts/ptyxis.sh
@@ -3,7 +3,7 @@
 set -oue pipefail
 
 # Add Staging repo
-wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-40/ublue-os-staging-fedora-40.repo -O /etc/yum.repos.d/ublue-os-staging-fedora-40.repo
+wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-41/ublue-os-staging-fedora-41.repo -O /etc/yum.repos.d/ublue-os-staging-fedora-41.repo
 
 # 40 gets Ptyxis and patched Mutter
   #  rpm-ostree override replace \
@@ -14,4 +14,4 @@ wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-40/ubl
   #  rpm-ostree install ptyxis
             
 # Add Nerd Fonts
-wget https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-40/che-nerd-fonts-fedora-40.repo -O /etc/yum.repos.d/_copr_che-nerd-fonts-40.repo
+wget https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-41/che-nerd-fonts-fedora-41.repo -O /etc/yum.repos.d/_copr_che-nerd-fonts-41.repo
